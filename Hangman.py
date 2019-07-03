628b400529321fa894c47d7bf1c55b25e3609e96
bigswimmer2 <brettpinfold2@gmail.com>
2019-07-03 15:54:02 -0500

    what the board will look like

--- Hangman.py
+++ Hangman.py
@@ -2,8 +2,23 @@
 
 print ("Welcome To My Hangman Game")
 print ("--------------------------")
+line1 =" ----"
+line2 =" |   |"
+line3 =" |   O"
+line4 =" |  -|-"
+line5 ="_|_ / \ "
+
+def print_board(tries):
+    print(line1)
+    print(line2)
+    print(line3)
+    print(line4)
+    print(line5)
+    return
+
 
 user_input = "yes"
 while user_input == "yes":
     user_input = input("Do you want to go again? yes or no: ")
+    print_board()
 
