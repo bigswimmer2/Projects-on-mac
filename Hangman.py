df13112aa4dd286ce6662a148cb3d64047b03a4c
bigswimmer2 <brettpinfold2@gmail.com>
2019-07-03 14:43:39 -0500

    start

---
+++ Hangman.py
@@ -0,0 +1,9 @@
+
+
+print ("Welcome To My Hangman Game")
+print ("--------------------------")
+
+user_input = "yes"
+while user_input == "yes":
+    user_input = input("Do you want to go again? yes or no: ")
+
